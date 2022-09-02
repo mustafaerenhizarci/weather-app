113b291cbfc6438ddbcea5bf19cedeaf30276cc1
mustafaerenhizarci <mrgenerator3@gmail.com>
2022-09-02 03:25:29 +0300

    bug fix

--- src/context/WeatherContext.jsx
+++ src/context/WeatherContext.jsx
@@ -1,6 +1,6 @@
 import { createContext, useEffect, useState } from "react";
 import * as Location from "expo-location";
-import  Constants  from "expo-constants";
+import Constants from "expo-constants";
 
 export const WeatherContext = createContext();
 
@@ -15,7 +15,7 @@ export default function WeatherProvider({ children }) {
     "Cumartesi",
   ];
 
-  const API_KEY = Constants.manifest.extra.environment['API_KEY'];
+  const API_KEY = Constants.manifest.extra.environment["API_KEY"];
   const [location, setLocation] = useState(0);
   const [storedLocations, setStoredLocations] = useState([]);
   const [lang, setLang] = useState("tr");
